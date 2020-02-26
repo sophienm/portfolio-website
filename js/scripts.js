c415e070fcab656ce79c32a574a312c250376f9b
sophienm <60009929+sophienm@users.noreply.github.com>
2020-02-26 18:44:21 +0100

    add tooltips

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 console.log('Hi! Welcome to my portfolio website.')
 
-function menuToggle() {
+function menuToggle () {
   var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
     x.className += ' responsive'
@@ -8,3 +8,84 @@ function menuToggle() {
     x.className = 'navtoggle'
   }
 }
+
+const button = document.querySelector('#button');
+      const tooltip = document.querySelector('#tooltip');
+
+      Popper.createPopper(button, tooltip);
+
+      function show() {
+  tooltip.setAttribute('data-show', '');
+}
+
+function hide() {
+  tooltip.removeAttribute('data-show');
+}
+
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
+
+showEvents.forEach(event => {
+  button.addEventListener(event, show);
+});
+
+hideEvents.forEach(event => {
+  button.addEventListener(event, hide);
+});
+
+let popperInstance = null;
+
+function create() {
+  popperInstance = Popper.createPopper(button, tooltip, {
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 8],
+        },
+      },
+    ],
+  });
+}
+
+function destroy() {
+  if (popperInstance) {
+    popperInstance.destroy();
+    popperInstance = null;
+  }
+}
+
+function show() {
+  tooltip.setAttribute('data-show', '');
+  create();
+}
+
+function hide() {
+  tooltip.removeAttribute('data-show');
+  destroy();
+}
+
+
+
+
+function generateGetBoundingClientRect(x = 0, y = 0) {
+  return () => ({
+    width: 0,
+    height: 0,
+    top: y,
+    right: x,
+    bottom: y,
+    left: x,
+  });
+}
+
+const virtualElement = {
+  getBoundingClientRect: generateGetBoundingClientRect(),
+};
+
+const instance = createPopper(virtualElement, popper);
+
+document.addEventListener('mousemove', ({ clientX: x, clientY: y }) => {
+  virtualElement.getBoundingClientRect = generateGetBoundingClientRect(x, y);
+  instance.update();
+});
